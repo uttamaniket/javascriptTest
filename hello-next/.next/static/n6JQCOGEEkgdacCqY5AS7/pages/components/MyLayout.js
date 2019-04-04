987c4e5d71d1151059fd48e6b547923d2621faba
uttamaniket <aniketuttam11@gmail.com>
2019-04-04 18:14:26 +0530

    learn formik

---
+++ hello-next/.next/static/n6JQCOGEEkgdacCqY5AS7/pages/components/MyLayout.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{myj8:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/MyLayout",function(){var n=w("Mp32");return{page:n.default||n}}])}},[["myj8",1,0]]]);
--- hello-next/pages/components/MyLayout.js
+++ hello-next/pages/components/MyLayout.js
@@ -1,4 +1,5 @@
 import Header from './Header'
+import page from '..';
 
 const layoutStyle = {
   margin: 20,
