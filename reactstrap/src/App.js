fd338815d7d28c403907745c89ac3500494ed7fa
uttamaniket <aniketuttam11@gmail.com>
2019-04-17 17:05:03 +0530

    leaning reactstrap

--- reactstrap/src/App.js
+++ reactstrap/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Example from './Nav';
 import 'bootstrap/dist/css/bootstrap.css';
+import Alertreact from './alert';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Example />
+        <Alertreact />
       </div>
     );
   }
